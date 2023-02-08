0b59b751da0094ae924b92b8f74c867f74bdece3
Ubuntu Linux - Kishan <yogasewa2@gmail.com>
2023-02-08 19:07:44 +0530

    remove color theme

--- js/color-theme.js
+++
@@ -1,5 +0,0 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.body.classList.add('dark');
-} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
-    document.body.classList.remove('dark');
-}
